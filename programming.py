de47f8365ed4ab51ffeb49b9f887e6caaefee560
SUDEEP B M <142051243+sudeepbm@users.noreply.github.com>
2025-06-17 23:38:38 +0530

    Day 2

--- programming.py
+++ programming.py
@@ -44,3 +44,64 @@ b=20
 print(f"Before swap: \na:{a}\tb:{b}")
 a,b=b,a
 print(f"After swap: \na:{a}\tb:{b}")
+
+
+
+# Day-2                     Date: 14-06-2025
+
+# Swapping of 2 numbers
+
+# a=int(input("a: "))
+# b=int(input("b: "))
+# c=int(input("c: "))
+# logic 1
+a=10
+b=20
+c=30
+d=a
+a=c
+c=b
+b=d
+print(f"a: {a}\tb: {b}\tc: {c}")
+
+# logic 2
+a=10
+b=20
+c=30
+a=a+b+c
+b=a-b-c
+c=a-b-c
+a=a-b-c
+print(f"a: {a}\tb: {b}\tc: {c}")
+
+# logic 3
+a=10
+b=20
+c=30
+a=a*b*c
+# b=a//b//c
+# c=a//b//c
+# a=a//b//c
+b=a//(b*c)
+c=a//(b*c)
+a=a//(b*c)
+print(f"a: {a}\tb: {b}\tc: {c}")
+
+# logic 4
+a=10
+b=20
+c=30
+a=a^b^c
+b=a^b^c
+c=a^b^c
+a=a^b^c
+print(f"a: {a}\tb: {b}\tc: {c}")
+
+# logic 5
+a=10
+b=20
+c=30
+a,b,c=c,a,b
+print(f"a: {a}\tb: {b}\tc: {c}")
+
+# checking if the number is even or odd
