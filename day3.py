24c89444fce784b1a4130f3ac3ba8fef1cda504f
SUDEEP B M <142051243+sudeepbm@users.noreply.github.com>
2025-06-17 23:31:37 +0530

    Updated day3.py

--- day3.py
+++ day3.py
@@ -1,2 +1,23 @@
+# Day-3                     Date: 16-06-2025
+
+
 # Pattern Programming
 
+print("*")
+
+
+print("*")
+print("*")
+
+
+print("*")
+print("*")
+print("*")
+
+for i in range(3):
+    print("*")
+
+
+n=int(input("n: "))
+for i in range(n):
+    print("*")
