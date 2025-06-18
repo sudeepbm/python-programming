81437bd6adba81dee22102d2b405540d56b80c0d
SUDEEP B M <142051243+sudeepbm@users.noreply.github.com>
2025-06-18 23:59:58 +0530

    Day 5

---
+++ day5.py
@@ -0,0 +1,8 @@
+row=int(input("Enter number of rows: "))
+col=int(input("Enter number of columns: "))
+for i in range(row):
+    val=col
+    for j in range(col):
+        print(val, end=" ")
+        val-=1
+    print()  # Move to the next line after each row is printed
