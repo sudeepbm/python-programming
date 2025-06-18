9a3b216b10603d1d38717980abe14168d7f3d1a6
SUDEEP B M <142051243+sudeepbm@users.noreply.github.com>
2025-06-19 00:05:08 +0530

    Day 5

--- programming.py
+++ programming.py
@@ -195,3 +195,16 @@ for i in range(row):
     val-=1
     if val<1:
         val=9
+
+
+
+# Day-5                     Date: 18-06-2025
+
+row=int(input("Enter number of rows: "))
+col=int(input("Enter number of columns: "))
+for i in range(row):
+    val=col
+    for j in range(col):
+        print(val, end=" ")
+        val-=1
+    print()  # Move to the next line after each row is printed
