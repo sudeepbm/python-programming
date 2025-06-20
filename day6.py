59a7e53f3528951de6e46cabbb33e22496b1465c
SUDEEP B M <142051243+sudeepbm@users.noreply.github.com>
2025-06-20 23:54:19 +0530

    Day 6

---
+++ day6.py
@@ -0,0 +1,14 @@
+# Day-5                     Date: 18-06-2025
+
+# row=int(input("Enter number of rows: "))
+# col=int(input("Enter number of columns: "))
+
+n = 5  # Size of the pattern (must be odd for symmetry)
+
+for i in range(n):
+    for j in range(n):
+        if i == j or i + j == n - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
