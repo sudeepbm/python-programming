6d91ecdce621c5b2fdf63cf2eeca492b1a4ba048
SUDEEP B M <142051243+sudeepbm@users.noreply.github.com>
2025-06-25 22:43:51 +0530

    Updated day10.py

--- day10.py
+++ day10.py
@@ -1,3 +1,28 @@
 # Day-10                     Date: 24-06-2025
 
 # Number Series
+
+# Example 1
+n=123
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+
+# Example 2
+n=81953
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
