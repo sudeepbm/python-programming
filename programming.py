fe45fd2db9f800d33408a4c0991ab44390b7d45f
SUDEEP B M <142051243+sudeepbm@users.noreply.github.com>
2025-06-25 22:45:14 +0530

    Day 10

--- programming.py
+++ programming.py
@@ -1203,3 +1203,34 @@ for i in range(n):
         else:
             print(" ",end=" ")
     print()
+
+
+
+# Day-10                     Date: 24-06-2025
+
+# Number Series
+
+# Example 1
+n=123
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+
+# Example 2
+n=81953
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
+n=n//10
+print(n%10)
